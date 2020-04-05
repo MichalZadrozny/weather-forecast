2a3f7553c63d6bf8aad15e7e8aeab39fef0eb0cc
MichalZadrozny <>
2020-04-06 01:24:35 +0200

    Changed search box appearance and fixed displaying weather images

--- src/main/java/pl/michalzadrozny/weatherforecast/config/WeatherDisplay.java
+++ src/main/java/pl/michalzadrozny/weatherforecast/config/WeatherDisplay.java
@@ -11,7 +11,7 @@ import pl.michalzadrozny.weatherforecast.model.CurrentWeather;
 @Setter
 @Getter
 @ToString
-public class WeatherDisplay{
+public class WeatherDisplay {
 
     CurrentWeather currentWeather;
 
@@ -20,30 +20,30 @@ public class WeatherDisplay{
         this.currentWeather = currentWeather;
     }
 
-    public void setTemperatures(boolean isSetAsCelsius){
-        if(isSetAsCelsius){
-            currentWeather.setCurrentTemperature(currentWeather.getMain().kelvinToCelsius(currentWeather.getMain().getTemp())+" °C");
-            currentWeather.setMinTemperature(currentWeather.getMain().kelvinToCelsius(currentWeather.getMain().getTempMin())+" °C");
-            currentWeather.setMaxTemperature(currentWeather.getMain().kelvinToCelsius(currentWeather.getMain().getTempMax())+" °C");
+    public void setTemperatures(boolean isSetAsCelsius) {
+        if (isSetAsCelsius) {
+            currentWeather.setCurrentTemperature(currentWeather.getMain().kelvinToCelsius(currentWeather.getMain().getTemp()) + " °C");
+            currentWeather.setMinTemperature(currentWeather.getMain().kelvinToCelsius(currentWeather.getMain().getTempMin()) + " °C");
+            currentWeather.setMaxTemperature(currentWeather.getMain().kelvinToCelsius(currentWeather.getMain().getTempMax()) + " °C");
 
-            System.out.println("Celsius: "+currentWeather.getCurrentTemperature());
-        }else{
-            currentWeather.setCurrentTemperature(currentWeather.getMain().kelvinToFahrenheit(currentWeather.getMain().getTemp())+" °F");
-            currentWeather.setMinTemperature(currentWeather.getMain().kelvinToFahrenheit(currentWeather.getMain().getTempMin())+" °F");
-            currentWeather.setMaxTemperature(currentWeather.getMain().kelvinToFahrenheit(currentWeather.getMain().getTempMax())+" °F");
-            System.out.println("Fahrenheit: "+currentWeather.getCurrentTemperature());
+            System.out.println("Celsius: " + currentWeather.getCurrentTemperature());
+        } else {
+            currentWeather.setCurrentTemperature(currentWeather.getMain().kelvinToFahrenheit(currentWeather.getMain().getTemp()) + " °F");
+            currentWeather.setMinTemperature(currentWeather.getMain().kelvinToFahrenheit(currentWeather.getMain().getTempMin()) + " °F");
+            currentWeather.setMaxTemperature(currentWeather.getMain().kelvinToFahrenheit(currentWeather.getMain().getTempMax()) + " °F");
+            System.out.println("Fahrenheit: " + currentWeather.getCurrentTemperature());
 
         }
     }
 
-    public boolean changeToCelsius(){
+    public boolean changeToCelsius() {
         System.out.println("Before changing to Celsius");
         setTemperatures(true);
         System.out.println("Changed to Celsius");
         return true;
     }
 
-    public boolean changeToFahrenheit(){
+    public boolean changeToFahrenheit() {
         System.out.println("Before changing to Fahrenheit");
 
         setTemperatures(false);
@@ -51,28 +51,29 @@ public class WeatherDisplay{
         return false;
     }
 
-    public String getWeatherImage(){
-        System.out.println("Indide getWeatherImage");
+    public String getWeatherImage() {
 
         int id = currentWeather.getWeather().get(0).getId();
-        if(id >= 200 && id < 300){
-            return "thunderstorm";
-        }else if(id >= 300 && id < 400){
-            return "dizzle";
-        }else if(id >= 500 && id < 600){
-            return "rain";
-        }else if(id >= 600 && id < 700){
-            return "snow";
-        }else if(id >= 700 && id < 800){
-            return "atmosphere";
-        }else if(id == 800){
-            return "clear";
-        }else if(id == 801 || id == 802){
-            return "partly-cloudy";
-        }else if(id == 803 || id == 804){
-            return "cloudy";
-        }else{
-            return "error";
+        if (id >= 200 && id < 300) {
+            return "img/weather/thunderstorm";
+        } else if (id >= 300 && id < 400) {
+            return "img/weather/rain";
+        } else if (id >= 500 && id < 600) {
+            return "img/weather/rain";
+        } else if (id >= 600 && id < 700) {
+            return "img/weather/snow";
+        } else if (id == 781) {
+            return "img/weather/tornado";
+        } else if (id >= 700 && id < 800) {
+            return "img/weather/atmosphere";
+        } else if (id == 800) {
+            return "img/weather/clear";
+        } else if (id == 801 || id == 802) {
+            return "img/weather/partly-cloudy";
+        } else if (id == 803 || id == 804) {
+            return "img/weather/cloudy";
+        } else {
+            return null;
         }
     }
 
