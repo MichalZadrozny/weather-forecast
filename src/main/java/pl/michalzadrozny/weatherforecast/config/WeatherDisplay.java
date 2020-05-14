60db437ab6b9abf1232932e395c40c0f43f53ef2
Michał Zadrożny <mt.zadrozny@gmail.com>
2020-05-14 19:56:08 +0200

    Changed passing city from parameter to argument

--- src/main/java/pl/michalzadrozny/weatherforecast/config/WeatherDisplay.java
+++ src/main/java/pl/michalzadrozny/weatherforecast/config/WeatherDisplay.java
@@ -22,27 +22,9 @@ public class WeatherDisplay {
         this.currentWeather = currentWeather;
     }
 
-    //    public void setTemperatures(boolean isSetAsCelsius) {
-//        if (isSetAsCelsius) {
-//            currentWeather.setCurrentTemperature(currentWeather.getMain().kelvinToCelsius(currentWeather.getMain().getTemp()) + " °C");
-//            currentWeather.setMinTemperature(currentWeather.getMain().kelvinToCelsius(currentWeather.getMain().getTempMin()) + " °C");
-//            currentWeather.setMaxTemperature(currentWeather.getMain().kelvinToCelsius(currentWeather.getMain().getTempMax()) + " °C");
-//            currentWeather.setPerceptibleTemperature(currentWeather.getMain().kelvinToCelsius(currentWeather.getMain().getFeelsLike()) + " °C");
-//
-//
-//            System.out.println("Celsius: " + currentWeather.getCurrentTemperature());
-//        } else {
-//            currentWeather.setCurrentTemperature(currentWeather.getMain().kelvinToFahrenheit(currentWeather.getMain().getTemp()) + " °F");
-//            currentWeather.setMinTemperature(currentWeather.getMain().kelvinToFahrenheit(currentWeather.getMain().getTempMin()) + " °F");
-//            currentWeather.setMaxTemperature(currentWeather.getMain().kelvinToFahrenheit(currentWeather.getMain().getTempMax()) + " °F");
-//            currentWeather.setPerceptibleTemperature(currentWeather.getMain().kelvinToFahrenheit(currentWeather.getMain().getFeelsLike()) + " °F");
-//            System.out.println("Fahrenheit: " + currentWeather.getCurrentTemperature());
-//
-//        }
-//    }
     public void setTemperatures(boolean isSetAsCelsius) {
         if (isSetAsCelsius) {
-            currentWeather.setCurrentTemperature( round(currentWeather.getMain().getTemp()) + " °C");
+            currentWeather.setCurrentTemperature(round(currentWeather.getMain().getTemp()) + " °C");
             currentWeather.setMinTemperature(round(currentWeather.getMain().getTempMin()) + " °C");
             currentWeather.setMaxTemperature(round(currentWeather.getMain().getTempMax()) + " °C");
             currentWeather.setPerceptibleTemperature(round(currentWeather.getMain().getFeelsLike()) + " °C");
@@ -57,21 +39,6 @@ public class WeatherDisplay {
             System.out.println("Fahrenheit: " + currentWeather.getCurrentTemperature());
         }
     }
-//
-//    public boolean changeToCelsius() {
-//        System.out.println("Before changing to Celsius");
-//        setTemperatures(true);
-//        System.out.println("Changed to Celsius");
-//        return true;
-//    }
-//
-//    public boolean changeToFahrenheit() {
-//        System.out.println("Before changing to Fahrenheit");
-//
-//        setTemperatures(false);
-//        System.out.println("Changed to Fahrenheit");
-//        return false;
-//    }
 
     public String getWeatherImage() {
 
--- src/main/java/pl/michalzadrozny/weatherforecast/controller/WeatherForecastController.java
+++ src/main/java/pl/michalzadrozny/weatherforecast/controller/WeatherForecastController.java
@@ -6,7 +6,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import pl.michalzadrozny.weatherforecast.config.WeatherDisplay;
 import pl.michalzadrozny.weatherforecast.model.CurrentWeather;
 import pl.michalzadrozny.weatherforecast.service.ApiService;
@@ -18,7 +20,6 @@ public class WeatherForecastController {
     private ApiService apiService;
     private WeatherDisplay weatherDisplay;
     private boolean isSetAsCelsius;
-    private String city;
     private RestTemplate restTemplate;
 
 
@@ -40,20 +41,16 @@ public class WeatherForecastController {
     }
 
     @PostMapping("/")
-    public String sendCityName(String city, Model model){
-        model.addAttribute("weatherDisplay",weatherDisplay);
-        this.city = city;
+    public String sendCityName(RedirectAttributes redirectAttributes, String city){
+        redirectAttributes.addAttribute("city",city);
+
         return "redirect:/weather";
     }
 
-    @PostMapping("/weather")
-    public String resendCityName(String city, Model model){
-        this.city = city;
-        return "redirect:/weather";
-    }
+
 
     @GetMapping("/weather")
-    public String showWeatherPage(Model model) {
+    public String showWeatherPage(@RequestParam String city, Model model) {
         try{
             CurrentWeather currentWeather = restTemplate.getForObject(apiService.createUri(city,"metric"),CurrentWeather.class);
 
@@ -65,8 +62,7 @@ public class WeatherForecastController {
 
             return "weather";
         }catch (Exception e){
-            e.printStackTrace();
-            System.out.println(e.getStackTrace());
+            log.error(String.valueOf(e.getMessage()));
         }
 
         return "index";
